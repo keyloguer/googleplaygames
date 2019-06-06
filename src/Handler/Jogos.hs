2b9f9ef6714ff783cdde44a9dd58c776c547f59c
Daniel Pereira Zitei <daniel.zitei@gmail.com>
2019-06-06 13:06:19 +0000

    innerjoin jogos - Daniel

--- src/Handler/Jogos.hs
+++ src/Handler/Jogos.hs
@@ -35,3 +35,19 @@ getJogosR = do
                                             cla <- get404 claid
                                             gen  <- get404 genid
                                             return $ (dev,cla,gen)
+
+getJogosDevR :: Handler Value
+getJogosDevR = do
+        addHeader "Access-Control-Allow-Origin" "*"
+        jogos <- runDB $ selectList [] [Asc JogosTitulo, LimitTo 500]
+        jogosComInnerJoin <- mapM (\ jogo@(Entity jid (Jogos did _ _ _ _ _ _ _ genid clid _ _ _ _ _)) -> do 
+                             (dev, cla, gen) <- select did clid genid
+                             return $ JogosData (entityVal jogo) dev cla gen
+                             ) jogos
+        sendStatusJSON ok200 (toJSON $ jogosComInnerJoin)
+        where select :: DesenvolvedoresId -> ClassificacoesId -> GenerosId -> Handler (Desenvolvedores,Classificacoes,Generos)
+              select did claid genid = runDB $ do 
+                                            dev <- get404 did 
+                                            cla <- get404 claid
+                                            gen  <- get404 genid
+                                            return $ (dev,cla,gen)
